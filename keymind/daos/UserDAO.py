23ce432e0ef62dce8530c1ad2842879092d429fa
KEYDATA\umit.karaduman <umit.karaduman@keydata.com.tr>
2019-12-04 11:41:04 +0300

    Comment'ler eklendi

--- keymind/daos/UserDAO.py
+++ keymind/daos/UserDAO.py
@@ -1,12 +1,16 @@
 from keymind.daos.AbstractDAO import AbstractDAO
 from keymind.models.User import User
 
-
+"""
+User nesnesi için veritabanı işlemlerinin yapıldığı metodları içerir
+"""
 class UserDAO(AbstractDAO):
 
+    # username değerine göre User nesnesini veritabanından getiren metod
     def find_by_username(self, username):
         return User.query.filter_by(username=username).first()
 
+    # _id değerine göre User nesnesini veritabanından getiren metod
     def find_by_id(self, _id):
         return User.query.filter_by(id=_id).first()
 
--- keymind/models/User.py
+++ keymind/models/User.py
@@ -1,20 +1,25 @@
 from db import db
 
-
+"""
+User tablosu için veritabı eşleştirmelerinin yapıldığı model sınıfı
+"""
 class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
-    # role = db.Column(db.String(80))
+    role = db.Column(db.String(80))
 
-    def __init__(self, username, password):
+    def __init__(self, username:str, password:str, role:int):
         self.username = username
         self.password = password
+        self.role = role
 
+    # Nesneyi json'a çeviren metod
     def json(self):
         return {
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            'role': self.role
         }
