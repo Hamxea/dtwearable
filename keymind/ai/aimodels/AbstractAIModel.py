83f77a8a9a58318d50a3ab42785cf87a2cbfd248
KEYDATA\umit.karaduman <umit.karaduman@keydata.com.tr>
2019-12-09 16:41:14 +0300

    ARGE05-50

--- keymind/ai/aimodels/AbstractAIModel.py
+++ keymind/ai/aimodels/AbstractAIModel.py
@@ -2,6 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class AbstractAIModel(ABC):
+    """ AI Model sınıflarının üst sınıfı olarak kullanılan Abstract sınıf """
 
     @abstractmethod
-    def train(self, dataset_parameters, hyperparameters): pass
+    def train(self, dataset_parameters, hyperparameters):
+        """ Model'in eğitim için kullandığı train metodu, dataset ve hyper parametre detayları alır """
+
+        pass
