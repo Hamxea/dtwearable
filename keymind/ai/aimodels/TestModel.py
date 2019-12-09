99c0258c03298aface2c19dfcfa523d6379e67bd
KEYDATA\umit.karaduman <umit.karaduman@keydata.com.tr>
2019-12-09 16:43:26 +0300

    ARGE05-50

--- keymind/ai/aimodels/TestModel.py
+++ keymind/ai/aimodels/TestModel.py
@@ -1,9 +1,12 @@
 from keymind.ai.aimodels.AbstractAIModel import AbstractAIModel
 
 
-class TestModel():
+class TestModel(AbstractAIModel):
+    """ AI Model algoritmalarını test etmek için oluşturulan test sınıfı """
 
     def train(self, dataset_parameters, hyperparameters):
+        """ Test amaçlı boşcevap dönen metod """
+
         print("train()")
         print("dataset_parameters", dataset_parameters)
         print("hyperparameters", hyperparameters)
