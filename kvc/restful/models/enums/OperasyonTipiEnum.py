97c61d8b6b943fc5c701b050227acf6eb12678b0
Selim Surucu <selim.surucu@keydata.com.tr>
2019-12-11 15:26:55 +0300

    ARGE05-206 İslem Operasyon

---
+++ kvc/restful/models/enums/OperasyonTipiEnum.py
@@ -0,0 +1,5 @@
+from kvc.restful.models.enums import IntEnum
+
+class OperasyonTipiEnum(IntEnum):
+    ANA_ISLEM = 0,
+    EK_ISLEM = 1
---
+++ kvc/restful/resources/IslemOperasyonRegisterResource.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+
+from flask_restful import reqparse, Resource
+from kvc.restful.daos.IslemOperasyonDAO import IslemOperasyonDAO
+from kvc.restful.models.IslemOperasyon import IslemOperasyon
+
+class IslemOperasyonRegisterResource(Resource) :
+    """
+    Islem Operasyon nesnesi için parametre almayan metodları barındıran Resource sınıfı
+    Restful istek tiplerine karşılık metodlar oluşturulur
+    """
+
+    """ Restful isteklerini tanımlamak icin olusturulur, uyumsuzluk halinde hata donmesi saglanır """
+
+    islem_operasyon_post_parser = reqparse.RequestParser()
+    islem_operasyon_post_parser.add_argument('id',
+                                            type=int,
+                                            required=False,
+                                            )
+    islem_operasyon_post_parser.add_argument('islem_id',
+                                            type=int,
+                                            required=False,
+                                            )
+    islem_operasyon_post_parser.add_argument('operasyon_sut',
+                                             type=str,
+                                             required=False,
+                                             )
+    islem_operasyon_post_parser.add_argument('operasyon_tipi',
+                                             type=int,
+                                             required=False,
+                                             )
+    islemOperasyonDAO = IslemOperasyonDAO()
+
+    def post(self):
+        """ Restful isteğinin body kısmında bulunan veriye gore Hemsire Gözlem nesnesini olusturan ve veritabanına yazan metod """
+        data = self.islem_operasyon_post_parser.parse_args()
+        islem_operasyon = IslemOperasyon(**data)
+        try:
+            self.islemOperasyonDAO.save_to_db(islem_operasyon)
+        except Exception as e:
+            return {"message": "An error occurred while inserting the item. ",
+                    "exception": e
+                    }, 500
+
+        return islem_operasyon.serialize, 201
+    def put(self):
+        """ Restful isteğinin body kısmında bulunan veriye gore Hemsire Gözlem nesnesini olusturan veya guncelleyen metod """
+        data = self.islem_operasyon_post_parser.parse_args()
+
+        islem_operasyon = self.islemOperasyonDAO.find_by_id(data['id'])
+        if islem_operasyon:
+            islem_operasyon.islem_id = data['islem_id']
+            islem_operasyon.operasyon_sut = data['operasyon_sut']
+            islem_operasyon.operasyon_tipi = data['operasyon_tipi']
+        else:
+            islem_operasyon = IslemOperasyon(**data)
+
+        self.islemOperasyonDAO.save_to_db(islem_operasyon)
+
+        return islem_operasyon.serialize
