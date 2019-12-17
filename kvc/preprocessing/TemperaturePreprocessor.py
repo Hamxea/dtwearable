c2afead267e60fdc4830b29ce51a8b010bc2d5f1
ismetb <ismetbahadir@gmail.com>
2019-12-17 09:16:49 +0300

    ARGE05-210 Kodlama tamamlandı. window_size kadar olan diziler oluşturulup bir dataframe içinde dönülüyor.

--- kvc/preprocessing/TemperaturePreprocessor.py
+++ kvc/preprocessing/TemperaturePreprocessor.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import pandas as pd
 
 class TemperaturePreprocessor():
@@ -6,7 +8,55 @@ class TemperaturePreprocessor():
     def __init__(self):
         print()
 
-    def preprocess(self, df):
-        print("sdsd")
-        return None
+    def preprocess(self, my_list, time_interval_in_hours=2):
+        pass
+
+    def differentiate_by_islem_id(self, my_list, time_interval_in_hours=2):
+        my_list.sort(key=lambda x: x.olcum_tarihi)
+        my_dict = {}
+        for i in range(len(my_list)):
+            hemsire_gozlem_dto = my_list[i]
+            if hemsire_gozlem_dto.islem_id not in my_dict:
+                my_dict[hemsire_gozlem_dto.islem_id] = []
+            inner_list = my_dict[hemsire_gozlem_dto.islem_id]
+
+            if len(inner_list) < 1:
+                inner_list.append([])
+
+            last_windowed_element = inner_list[-1]
+
+            if len(last_windowed_element) < 1:
+                last_windowed_element.append(hemsire_gozlem_dto)
+            else:
+                last_hemsire_gozlem = last_windowed_element[-1]
+                if hemsire_gozlem_dto.olcum_tarihi - last_hemsire_gozlem.olcum_tarihi < timedelta(hours=time_interval_in_hours):
+                    last_windowed_element.append(hemsire_gozlem_dto)
+                else:
+                    new_windowed_element = [hemsire_gozlem_dto]
+                    inner_list.append(new_windowed_element)
+
+        return my_dict
+
+    def windowing(self, my_dict, window_size = 3, column_list=list("ABC")):
+        df = pd.DataFrame(columns=column_list)
+        for key in my_dict:
+
+            if len(my_dict[key][0]) > window_size-1:
+                counter = 0
+                for i in range(len(my_dict[key][0])):
+                    if counter + window_size > len(my_dict[key][0]):
+                        break
+                    temp_list = []
+                    for j in range(window_size):
+                        temp_list.append(my_dict[key][0][counter+j].vucut_sicakligi)
+                    counter += 1
+                    print(temp_list)
+                    df.loc[len(df)] = [temp_list][0]
+                    df.head()
+
+        df.head()
+        return  df
+
+
+
 
--- kvc/preprocessing/test_temperaturePreprocessor.py
+++ kvc/preprocessing/test_temperaturePreprocessor.py
@@ -1,6 +1,55 @@
+from datetime import datetime, timedelta
+from operator import itemgetter
 from unittest import TestCase
+
+from kvc.preprocessing.TemperaturePreprocessor import TemperaturePreprocessor
+from kvc.restful.models.HemsireGozlemDTO import HemsireGozlemDTO
 
 
 class TestTemperaturePreprocessor(TestCase):
-    def test_preprocess(self):
-        self.fail()
+
+    def test_differentiate_by_islem_id(self):
+        li = []
+        date = datetime.today()
+        li.append(HemsireGozlemDTO(1, 1, date - timedelta(hours=3), 36, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 1, date - timedelta(hours=1), 38, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 1, date - timedelta(hours=0), 38, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 2, date - timedelta(hours=5), 36, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 1, date - timedelta(hours=2), 37, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 3, date - timedelta(hours=4), 37, None, None, None, None, None, None, None, None))
+
+        temperature_preprocessor = TemperaturePreprocessor()
+        my_dict = temperature_preprocessor.differentiate_by_islem_id(li)
+
+        islem_id = 1
+        self.assertEqual(3, len(my_dict))
+        self.assertTrue(islem_id in my_dict)
+        self.assertEqual(4, len(my_dict[islem_id][-1]))
+
+        islem_id = 2
+        self.assertTrue(islem_id in my_dict)
+        self.assertEqual(1, len(my_dict[islem_id][-1]))
+
+        islem_id = 4
+        self.assertTrue(islem_id not in my_dict)
+
+    def test_windowing(self):
+        li = []
+        date = datetime.today()
+        li.append(HemsireGozlemDTO(1, 1, date - timedelta(hours=5), 36, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 1, date - timedelta(hours=4), 37, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 1, date - timedelta(hours=3), 38, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 1, date - timedelta(hours=2), 39, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 1, date - timedelta(hours=1), 40, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 2, date - timedelta(hours=5), 36, None, None, None, None, None, None, None, None))
+        li.append(HemsireGozlemDTO(1, 3, date - timedelta(hours=4), 37, None, None, None, None, None, None, None, None))
+
+        temperature_preprocessor = TemperaturePreprocessor()
+        my_dict = temperature_preprocessor.differentiate_by_islem_id(li)
+
+        df = temperature_preprocessor.windowing(my_dict)
+
+        self.assertEqual(3, len(df))
+        self.assertEqual(3, df.shape[0])
+        self.assertEqual(3, df.shape[1])
+        self.assertEqual(37, df['B'].iloc[0])
