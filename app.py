e1efa91473c018a0c67d701cafbeae7b678b000d
KEYDATA\umit.karaduman <umit.karaduman@keydata.com.tr>
2019-12-20 18:16:34 +0300

    ARGE05-210 test sınıfı eklendi

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ api.add_resource(IslemOperasyonRegisterResource, '/islemoperasyon')
 db.init_app(app)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True, host='0.0.0.0')
