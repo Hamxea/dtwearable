fc3fd42ab5e94b2cb31231822c91fd1609b4e724
Hamza Mohammed <hamza.mohammed@keydata.com.tr>
2019-12-23 12:01:59 +0300

    ARGE05-148 Anlık veya tahmin edilen hemşire gözlem sonuçlarına göre kural tabanlı uyarı veren yapı geliştirilmesi

---
+++ kvc/restful/services/HemsireGozlemService.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from ai.restful.services.RuleViolationService import RuleViolationService
+from kvc.restful.daos.HemsireGozlemDAO import HemsireGozlemDAO
+from kvc.restful.daos.IslemDAO import IslemDAO
+from kvc.restful.models.HemsireGozlemDTO import HemsireGozlemDTO
+from kvc.ruleengines.NabizRuleEngine import NabizRuleEngine
+from kvc.ruleengines.RuleViolationException import RuleViolationException
+from kvc.ruleengines.TemperatureRuleEngine import TemperatureRuleEngine
+
+
+class HemsireGozlemService():
+    """ """
+
+    hemsire_gozlem_dao = HemsireGozlemDAO()
+    islem_dao = IslemDAO()
+    temperature_rule_engine = TemperatureRuleEngine()
+    nabiz_rule_engine = NabizRuleEngine()
+    rule_violation_service = RuleViolationService()
+
+    def create_hemsire_gozlem(self, hemsire_gozlem):
+        """  """
+
+        try:
+            self.hemsire_gozlem_dao.save_to_db(hemsire_gozlem)
+            self.temperature_rule_engine.execute(hemsire_gozlem.islem_dto,  hemsire_gozlem.vucut_sicakligi)
+            self.nabiz_rule_engine.execute(hemsire_gozlem.islem_dto, hemsire_gozlem.islem_dto.yas, hemsire_gozlem.nabiz)
+        except RuleViolationException as e:
+            """ TODO  rule_violation_service düzeltecek...tek db save_to olması lazım"""
+            self.rule_violation_service.save_rule_violation_to_db(HemsireGozlemDTO.__tablename__, hemsire_gozlem.id,
+                                                                  None, e.rule_enum, None,
+                                                                  hemsire_gozlem.vucut_sicakligi, datetime.now())
+            self.rule_violation_service.save_rule_violation_to_db(HemsireGozlemDTO.__tablename__, hemsire_gozlem.id,
+                                                                  None, e.rule_enum, None,
+                                                                  hemsire_gozlem.nabiz, datetime.now())
---
+++ kvc/ruleengines/TemperatureRuleEngine.py
@@ -0,0 +1,18 @@
+from kvc.ruleengines.AbstractRuleEngine import AbstractRuleEngine
+from kvc.ruleengines.enums.TemperatureEnum import TemperatureEnum
+from kvc.ruleengines.RuleViolationException import RuleViolationException
+
+
+class TemperatureRuleEngine(AbstractRuleEngine):
+    """ """
+
+    def execute(self, islem_dto, temperature):
+        """ """
+        if temperature < 36:
+            raise RuleViolationException("Vucut sıcakliği 36 derecenın altına düştü", TemperatureEnum.DUSUK_ATES)
+
+        if temperature > 37.5 or temperature <= 40:
+            raise RuleViolationException("Vucut sıcakliği 37.5 derecenın üstüne çikti", TemperatureEnum.YUKSEK_ATES)
+
+        if temperature > 40:
+            raise RuleViolationException("Vucut sıcakliği 40 derecenın üstüne çikti", TemperatureEnum.COK_YUKSEK_ATES)
