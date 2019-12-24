0b5366433230a5f383c77e6adcac6123d9fc84f1
Hamza Mohammed <hamza.mohammed@keydata.com.tr>
2019-12-24 09:56:05 +0300

    ARGE05-148 Anlık veya tahmin edilen hemşire gözlem sonuçlarına göre kural tabanlı uyarı veren yapı geliştirilmesi

--- kvc/ruleengines/vitalbulgular/TansiyonRuleEngine.py
+++ kvc/ruleengines/vitalbulgular/TansiyonRuleEngine.py
@@ -16,12 +16,12 @@ class TansiyonRuleEngine():
         if (130 <= tansiyon_sistolik <= 139) or (80 <= tansiyon_diastolik <= 89):
             raise RuleViolationException("Tansiyon Yükseldi. HİPERTENSİYON ASAMA-1. "
                                          "TANSİYON SİSTOLİK 130 la 139 arasına çikti "
-                                         "ve TANSİYON DİASTOLİK 89 ile 80 arasına çikti", TansiyonEnum.YUKSEK_TANSIYON_HİPERTENSİYON_ASAMA_1)
+                                         "ve TANSİYON DİASTOLİK 89 ile 80 arasına çikti", TansiyonEnum.YUKSEK_TANSIYON_HIPERTENSIYON_ASAMA_1)
 
         if (tansiyon_sistolik >= 140) or (tansiyon_diastolik > 90):
             raise RuleViolationException("Tansiyon Yükseldi. HİPERTENSİYON ASAMA-2. "
                                          "TANSİYON SİSTOLİK 140 yükseldi "
-                                         "ve TANSİYON DİASTOLİK 90    ", TansiyonEnum.YUKSEK_TANSIYON_HİPERTENSİYON_ASAMA_2)
+                                         "ve TANSİYON DİASTOLİK 90    ", TansiyonEnum.YUKSEK_TANSIYON_HIPERTENSIYON_ASAMA_2)
 
         if (tansiyon_sistolik >= 180) and (tansiyon_diastolik > 120):
             raise RuleViolationException("Tansiyon YükseldiHİPERTENSİYON ASAMA-2. "
