fc0bdf29ebab18f3712b38cdcc174ebb2f491688
Hamza Mohammed <hamza.mohammed@keydata.com.tr>
2019-12-25 16:37:02 +0300

    ARGE05-148 Anlık veya tahmin edilen hemşire gözlem sonuçlarına göre kural tabanlı uyarı veren yapı geliştirilmesi

--- kvc/restful/services/HemsireGozlemService.py
+++ kvc/restful/services/HemsireGozlemService.py
@@ -6,6 +6,7 @@ from ai.restful.services.RuleViolationService import RuleViolationService
 from kvc.restful.daos.HemsireGozlemDAO import HemsireGozlemDAO
 from kvc.restful.daos.IslemDAO import IslemDAO
 from kvc.restful.models.HemsireGozlemDTO import HemsireGozlemDTO
+from kvc.restful.services.ChoosenTypeEnum import ChoosenTypeEnum
 from kvc.ruleengines.vitalbulgular.NabizRuleEngine import NabizRuleEngine
 from kvc.ruleengines.RuleViolationException import RuleViolationException
 from kvc.ruleengines.vitalbulgular.TansiyonRuleEngine import TansiyonRuleEngine
@@ -28,17 +29,43 @@ class HemsireGozlemService():
 
         try:
             self.hemsire_gozlem_dao.save_to_db(hemsire_gozlem)
-            self.temperature_rule_engine.execute(hemsire_gozlem.islem_dto,  hemsire_gozlem.vucut_sicakligi)
 
-            # TemperaturePredictionAIModel.__name__
-            prediction_value = self.prediction_service.make_prediction("ai.aimodels.TemperaturePredictionAIModel.TemperaturePredictionAIModel", HemsireGozlemDTO.__tablename__, hemsire_gozlem.id, )
-            self.temperature_rule_engine.execute(hemsire_gozlem.islem_dto, prediction_value)
+            rule_violation_exception_list = []
+            rule_violation_exception_list.extend(self.get_temperature_rule_violations(hemsire_gozlem))
+
+            self.rule_violation_service.save_rule_violations(rule_violation_list=rule_violation_exception_list)
 
         except RuleViolationException as e:
             """ TODO  rule_violation_service düzeltecek...tek db save_to olması lazım"""
             self.rule_violation_service.save_rule_violation_to_db(HemsireGozlemDTO.__tablename__, hemsire_gozlem.id,
                                                                   None, e.rule_enum.name, None,
                                                                   hemsire_gozlem.vucut_sicakligi, datetime.now())
+
+    def get_temperature_rule_violations(self, hemsire_gozlem):
+        temp_rule_violation_exception_list = []
+        temp_rule_violation_exception_list.extend(
+            self.temperature_rule_engine.execute(hemsire_gozlem.islem_dto, hemsire_gozlem.vucut_sicakligi,
+                                                 choosen_type=ChoosenTypeEnum.REAL))
+
+        # TemperaturePredictionAIModel.__name__
+        prediction_values = self.hemsire_gozlem_dao.get_feature_values_for_prediction(hemsire_gozlem.islem_no,
+                                                                                      column_name="vucut_sicakligi",
+                                                                                      window_size=TemperaturePredictionAIModel.window_size,
+                                                                                      time_interval_in_hours=TemperaturePredictionAIModel.time_interval_in_hours)
+
+        prediction_dto = self.prediction_service.make_prediction(
+            ai_model_class="ai.aimodels.TemperaturePredictionAIModel.TemperaturePredictionAIModel",
+            reference_table=HemsireGozlemDTO.__tablename__,
+            reference_id=hemsire_gozlem.id,
+            prediction_input=prediction_values)
+        temp_rule_violation_exception_list.append(
+            self.temperature_rule_engine.execute(hemsire_gozlem.islem_dto, prediction_dto.prediction_value,
+                                                 ChoosenTypeEnum.PREDICT))
+
+        return temp_rule_violation_exception_list
+
+
+
 """
         try:
             self.nabiz_rule_engine.execute(hemsire_gozlem.islem_dto.yas, hemsire_gozlem.nabiz)
