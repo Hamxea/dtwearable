af47b3952cd2f4b7ad3089340f4c97beb03f75d9
KEYDATA\umit.karaduman <umit.karaduman@keydata.com.tr>
2019-12-25 09:11:38 +0300

    logger ayarları değitirildi

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ api = Api(app)
 """ Gunicor ve app logging ayarları """
 gunicorn_logger = logging.getLogger('gunicorn.debug')
 app.logger.handlers = gunicorn_logger.handlers
-app.logger.setLevel(gunicorn_logger.level)
-# app.logger.setLevel(logging.WARNING)
+# app.logger.setLevel(gunicorn_logger.level)
+app.logger.setLevel(logging.INFO)
 
 app.logger.debug("this is a DEBUG message for test")
 app.logger.info("this is an INFO message for test")
