e9549af4b7c6682e4e911ad61ce84b3e8fc917b2
Hamza Mohammed <hamza.mohammed@keydata.com.tr>
2019-12-27 18:46:52 +0300

    ARGE05-142 Hastanın genel durumununa göre kural tabanlı uyarı veren yapı geliştirilmelidir

--- kvc/restful/services/SiviAlimiService.py
+++ kvc/restful/services/SiviAlimiService.py
@@ -5,6 +5,7 @@ from kvc.restful.daos.IslemDAO import IslemDAO
 from kvc.restful.daos.SiviAlimiDAO import SiviAlimiDAO
 from kvc.restful.models.SiviAlimiDTO import SiviAlimiDTO
 from kvc.ruleengines.RuleViolationException import RuleViolationException
+from kvc.ruleengines.hastagenelduramakural.HastaGenelDuramaRuleEngine import HastaGenelDuramaRuleEngine
 from kvc.ruleengines.sivitakipverileri.SiviTakipRuleEngine import SiviTakipRuleEngine
 
 
@@ -14,6 +15,7 @@ class SiviAlimiService():
     sivi_alimi_dao = SiviAlimiDAO()
     islem_dao = IslemDAO()
     sivi_alimi_rule_engine = SiviTakipRuleEngine()
+    hasta_genel_alimi_rule_enine = HastaGenelDuramaRuleEngine()
     rule_violation_service = RuleViolationService()
 
     def create_sivi_alimi(self, sivi_alimi):
@@ -33,9 +35,17 @@ class SiviAlimiService():
         """ sivi fark kural tabanlı"""
 
         sivi_alimi_rule_violation_list = []
-        sivi_alimi_rule_violation_list.extend( self.sivi_alimi_rule_engine.execute(sivi_alimi.islem_dto, sivi_farki=sivi_alimi.sivi_farki, yas=sivi_alimi.islem_dto.yas,
+        sivi_alimi_rule_violation_list.extend(self.sivi_alimi_rule_engine.execute(sivi_alimi.islem_dto, sivi_farki=sivi_alimi.sivi_farki, yas=sivi_alimi.islem_dto.yas,
                                                 tansiyon_sistolik=None, tansiyon_diastolik=None, choosen_type=None, reference_table=SiviAlimiDTO.__tablename__ ,
                                                                                    reference_id= sivi_alimi.id, prediction_id=None))
 
         return sivi_alimi_rule_violation_list
 
+    def get_tahmin_genel_hasta_durumu(self, sivi_alimi, hemsire_gozlem):
+        """" hastabın genel tahmın kural tabanı """
+        """TODO genel tahmin edice kural tabani ekelecek"""
+
+        genel_tahmin_rule_violation_list = []
+        #genel_tahmin_rule_violation_list.extend(self.hasta_genel_alimi_rule_enine)
+
+        return genel_tahmin_rule_violation_list
